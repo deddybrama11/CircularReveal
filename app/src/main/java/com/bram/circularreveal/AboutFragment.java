7936c913b8fb37449ee75c6a461a9935b78a7413
User Dedy <dedybramayadi@gmail.com>
2020-05-07 12:30:18 +0700

    error in after open activity

--- app/src/main/java/com/bram/circularreveal/AboutFragment.java
+++ app/src/main/java/com/bram/circularreveal/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.bram.circularreveal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -7,11 +9,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AboutFragment extends Fragment {
+    Button checkprofil;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message,container,false);
+        View view = inflater.inflate(R.layout.fragment_message,container,false);
+
+        checkprofil = view.findViewById(R.id.checkprofil);
+        checkprofil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://www.linkedin.com/in/dedy-bramayadi-419298178/"));
+                startActivity(i);
+            }
+        });
+        return  view;
+
+
     }
 }
